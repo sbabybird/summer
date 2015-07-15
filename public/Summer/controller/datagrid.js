0fd12c99794e840fbe8d7889c1c7ee32c4089c0f
Minjie Sun <sbabybird@gmail.com>
2015-07-15 16:33:51 +0800

    viewcontroller

--- public/Summer/controller/datagrid.js
+++ public/Summer/controller/datagrid.js
@@ -2,11 +2,5 @@ Ext.define('Summer.controller.DataGrid', {
 	extend: 'Ext.app.ViewController',
 	init: function () {
 		console.log('hello');
-	},
-	render: function () {
-		console.log('hello2');
-				},
-				itemclick: function (td, cellIndex) {
-		console.log(td);
-				}
+	}
 })
